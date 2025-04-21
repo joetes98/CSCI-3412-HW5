5092866be3d1f7ea60eaeae22daf8256ff0235dd
joetes98 <joseph.tesoriero@ucdenver.edu>
2025-04-21 15:40:10 -0400

    WIP

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-a = [(1,2), (3,4), (5,6), (7,8)]
-print(dict(a))
+myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+def square(x):
+    return x**2
+
+evens = list(filter(lambda x: x%2 == 0, myNumbers))
+print(evens)
